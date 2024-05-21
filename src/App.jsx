88b3d2f0257e2da4fad626c41f6cba71f79a4072
muddabirm <33209109+muddabirm@users.noreply.github.com>
2024-05-20 20:49:24 -0500

    created frontend elements and navbar with logo.

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './Components/Navbar'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <Navbar />
+      <h2>
+        Hi, my name is Moody Masood and im a passionate fullstack web developer.
+      </h2>
+    </div>
+    <section>
+      <h1>
+        skills
+      </h1>
+    </section>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,27 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import logo from '../assets/moody_transparent.png'
+import '../App.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+        <img className='logo' src={logo} alt="" />
+        <ul className='navlinks'>
+            <li>
+                <a href="#">About Me</a>
+            </li>
+            <li>
+                <a href="#">Contact Me</a>
+            </li>
+            <li>
+                <a href="#">My Work</a>
+            </li>
+            <li>
+                <a href="#">Resume</a>
+            </li>
+        </ul>   
+    </div>
+  )
+}
+ 
+export default Navbar
